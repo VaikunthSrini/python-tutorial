35c4d1eeb2c77f436182e18f8372d44e7e0be7b9
Vaikunth Srinivasan <133582522+VaikunthSrini@users.noreply.github.com>
2023-07-05 09:16:06 +0000

    new code

---
+++ area.py
@@ -0,0 +1,10 @@
+# rectangle area and perimeter generater
+
+l = int(input("What is the length of you rectangle : "))
+w = int(input("What is the width of your rectangle : "))
+
+m = f"**** The area of your rectangle is {l*w} ****"
+k = f"**** The perimeter of your rectangle is {l+l+w+w} ****"
+
+print(m)
+print(k)
